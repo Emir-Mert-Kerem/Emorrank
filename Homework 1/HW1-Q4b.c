e0a1040a1f70229de47e91f990f242aaa0a1bfaa
Emir-Mert-Kerem <mertkerem2022@gmail.com>
2025-03-23 21:12:25 +0300

    Update HW1-Q4b.c

--- Homework 1/HW1-Q4b.c
+++ Homework 1/HW1-Q4b.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <math.h>
 
+void computeHomeValue(void);
+
+int main() {
+    computeHomeValue();
+    
+    return 0;
+}
+
+void computeHomeValue(void) {
+    int popularity;
+    int size;
+    int homeValue;
+
+    printf("Enter popularity: ");
+    scanf("%d", &popularity);
+
+    printf("Enter size: ");
+    scanf("%d", &size);
+
+    homeValue = (pow(popularity, 3) + pow(size, 2)) * 10000;
+
+    printf("Your home value is: %d TL", homeValue);
+}
