a498e1efa0314ba878bf8352c490aea466de2f87
Emir-Mert-Kerem <mertkerem2022@gmail.com>
2025-03-23 21:17:07 +0300

    Update HW1-Q2.c

--- Homework 1/HW1-Q2.c
+++ Homework 1/HW1-Q2.c
@@ -1,21 +1,19 @@
-/* Addition program */ 
 #include <stdio.h>
 
-/* function main begins program execution */
-int main( void )
-{
-   int integer1; /* first number to be input by user */
-   int integer2; /* second number to be input by user */
-   int sum;      /* variable in which sum will be stored */ 
+int main( void ) {
+   int integer1;
+   int integer2;
+   int sum;
 
-   printf("Enter first integer\n"); /* prompt */
-   scanf("%d", &integer1);        /* read an integer */
+   printf("Enter first integer\n");
+   scanf("%d", &integer1);
 
-   printf("Enter second integer\n"); /* prompt */
-   scanf("%d", &integer2);           /* read an integer */
+   printf("Enter second integer\n");
+   scanf("%d", &integer2);
    
-   sum = integer1 + integer2; /* assign total to sum */
+   sum = integer1 + integer2;
 
-   printf("Sum is %d\n", sum); /* print sum */
-   return 0; /* indicate that program ended successfully */
-} /* end function main */
+   printf("Sum is %d\n", sum);
+   
+   return 0;
+}
